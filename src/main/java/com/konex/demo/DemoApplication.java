4b4d71ad8201ddc93eff7711b1db1bc12fc629de
ezedev <ezequiel.ivan.guzman@gmail.com>
2023-02-16 04:14:08 +0100

    demoAPplication, dependencies and properties

--- src/main/java/com/konex/demo/DemoApplication.java
+++ src/main/java/com/konex/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.konex.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EntityScan(basePackages = {"com.konex.demo.infrastructure.entity"})
+@EnableJpaRepositories(basePackages = "com.konex.demo.domain.repository")
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
